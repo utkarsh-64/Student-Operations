7095c67d4b7749d31dce1de83209a180c0feed27
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-02-05 00:35:56 +0530

    Implemented 'Add Student' option

--- Main.java
+++ Main.java
@@ -12,5 +12,22 @@ public class Main {
             System.out.println("Choose an option:");
             System.out.println("1. Add Student\n2. Display Students\n3. Update Student\n4. Remove Student\n5. Exit");
             int choice = Integer.parseInt(scan.nextLine());
+
+            switch (choice) {
+                case 1 -> {
+                    // Add new student
+                    System.out.println("Enter Name: ");
+                    String name = scan.nextLine();
+                    System.out.println("Enter PRN: ");
+                    long prn = Long.parseLong(scan.nextLine());
+                    System.out.println("Enter GPA: ");
+                    double gpa = Double.parseDouble(scan.nextLine());
+                    System.out.println("Enter Batch: ");
+                    String batch = scan.nextLine();
+                    System.out.println("Enter Branch: ");
+                    String branch = scan.nextLine();
+                    Student student = new Student(name, prn, gpa, batch, branch);
+                    operations.addStudents(student); // Add student to the list
+                }
     }
 }    
