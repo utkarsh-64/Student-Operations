f97f41d21e9bed6cc1ccc95a4472b0ac58c389bb
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-02-05 00:34:18 +0530

    Added Main class with main method and initial setup

--- Main.java
+++ Main.java
@@ -1,1 +1,10 @@
+//Main.java
 
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        StudentOps operations = new StudentOps();
+    }
+}    
