982712a7265f3102a0494624f8339a59106c10a0
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-02-04 23:56:57 +0530

    Set private fields for student details

--- Student.java
+++ Student.java
@@ -1,4 +1,9 @@
 //Student.java
 class Student{
-    
+    //Setting private fields for Name, PRN, GPA, Batch, Branch
+    private String name;
+    private long prn;
+    private double gpa;
+    private String batch;
+    private String branch;
 }
