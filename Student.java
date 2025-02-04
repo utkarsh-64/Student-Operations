217f13f4cefb5af41d380445227d5bcd2a76ecbe
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-02-04 23:59:41 +0530

    Added setter and getter method for name

--- Student.java
+++ Student.java
@@ -15,4 +15,13 @@ class Student{
         this.branch = branch;
         this.batch = batch;
     }
+
+    //Add setter and getter method for name
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
 }
