de04bd5b8da9fc611b2f3ee73cfac40ab7c84170
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-02-04 23:44:48 +0530

    Added addStudents method

--- StudentOps.java
+++ StudentOps.java
@@ -3,8 +3,13 @@ import java.util.* ;
 
 class StudentOps{
 	ArrayList<Student> students;
-    
+    //Create a constructor for StudentOps
     public StudentOps(){
         this.students = new ArrayList<>();
+    }
+    //Create addStudent method
+	public void addStudents(Student student){
+        students.add(student);
+        System.out.println("Student Added Successfully");
     }
 }
